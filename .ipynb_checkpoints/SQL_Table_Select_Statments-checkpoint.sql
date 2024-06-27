0f28f35d2d4029bb525119bbb0996dd5061bf817
MakRamsey <164778729+MakRamsey@users.noreply.github.com>
2024-06-27 17:20:08 -0400

    Final Upload for Project 2 (ETL) Submission Files

---
+++ .ipynb_checkpoints/SQL_Table_Select_Statments-checkpoint.sql
@@ -0,0 +1,13 @@
+--Select statements to verify proper table creation, data type declaration & correct data hydration/import
+
+--Selecting all from campaign
+SELECT * FROM campaign
+
+--Selecting all from contacts
+SELECT * FROM contacts
+
+--Selecting all from category
+SELECT * FROM category
+
+--Selecting all from subcategory
+SELECT * FROM subcategory
